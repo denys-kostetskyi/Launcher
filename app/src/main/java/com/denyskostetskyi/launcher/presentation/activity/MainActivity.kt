bb293492396dc3e7924266c79759c7d351f212df
Denys Kostetskyi <denys.kostetskyi@globallogic.com>
2024-09-27 14:09:23 +0300

    SystemInfoService binding in MainActivity

--- app/src/main/java/com/denyskostetskyi/launcher/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/denyskostetskyi/launcher/presentation/activity/MainActivity.kt
@@ -1,13 +1,34 @@
 package com.denyskostetskyi.launcher.presentation.activity
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.denyskostetskyi.launcher.R
+import com.denyskostetskyi.launcher.domain.model.SystemInfo
+import com.denyskostetskyi.launcher.presentation.service.SystemInfoService
 
 class MainActivity : AppCompatActivity() {
+    private var serviceBinder: SystemInfoService.ServiceBinder? = null
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val binder = service as SystemInfoService.ServiceBinder
+            binder.setCallback(SYSTEM_INFO_UPDATE_DELAY, ::onSystemInfoChanged)
+            serviceBinder = binder
+            Log.d(TAG, "Bound to ${name?.className}")
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            serviceBinder = null
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +38,28 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        bindSystemInfoService()
+    }
+
+    override fun onDestroy() {
+        unbindService(serviceConnection)
+        super.onDestroy()
+    }
+
+    private fun bindSystemInfoService() {
+        val intent = SystemInfoService.newIntent(this)
+        val result = bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+        if (!result) {
+            Log.d(TAG, "SystemInfoService bind failed")
+        }
+    }
+
+    private fun onSystemInfoChanged(systemInfo: SystemInfo) {
+        Log.d(TAG, systemInfo.toString())
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val SYSTEM_INFO_UPDATE_DELAY = 60_000L
     }
 }

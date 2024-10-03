2d39b14cb5ec217bbb4d9e25d79729018c18de74
Denys Kostetskyi <denys.kostetskyi@globallogic.com>
2024-10-03 19:24:15 +0300

    HourlyDataDto fix

--- weather-forecast/src/main/java/com/denyskostetskyi/weatherforecast/data/remote/dto/HourlyDataDto.kt
+++ weather-forecast/src/main/java/com/denyskostetskyi/weatherforecast/data/remote/dto/HourlyDataDto.kt
@@ -3,27 +3,10 @@ package com.denyskostetskyi.weatherforecast.data.remote.dto
 import com.google.gson.annotations.SerializedName
 
 data class HourlyDataDto(
+    @SerializedName("time")
+    val timeArray: List<String>,
     @SerializedName("temperature_2m")
-    val temperatureArray: DoubleArray,
-
+    val temperatureArray: List<Double>,
     @SerializedName("weather_code")
-    val weatherCodeArray: IntArray
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as HourlyDataDto
-
-        if (!temperatureArray.contentEquals(other.temperatureArray)) return false
-        if (!weatherCodeArray.contentEquals(other.weatherCodeArray)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = temperatureArray.contentHashCode()
-        result = 31 * result + weatherCodeArray.contentHashCode()
-        return result
-    }
-}
+    val weatherCodeArray: List<Int>,
+)
